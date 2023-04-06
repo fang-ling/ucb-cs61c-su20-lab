0070a85476a614db44086651416ba8be69531699
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-04-06 13:10:52 +0800

    lab01 - done

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,33 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+#define var __auto_type
+
 int ll_has_cycle(node *head) {
-    /* your code here */
+    /* 1. */
+    var tortoise = head;
+    var hare = head;
+
+    while (1) {
+        if (hare == NULL) { /* empty list check */
+            return 0;
+        }
+        if (hare -> next != NULL) {
+            if (hare -> next -> next != NULL) {
+                /* 2 */
+                hare = hare -> next -> next;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+
+        tortoise = tortoise -> next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
+
     return 0;
 }
