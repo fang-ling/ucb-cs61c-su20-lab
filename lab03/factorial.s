cbe323e434f7510f40e8362722186159b1177a6f
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-04-10 13:52:03 +0800

    [lab03] factorial done

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,23 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    # var i = n
+    # var result = 1
+    # while i > 0 {
+    #     result *= i
+    #     i -= 1
+    # }
+
+    add t0, x0, a0  # t0 = n (var i)
+    addi t1, x0, 1  # t1 = 1 (var result)
+
+    j Test
+Loop:
+    mul t1, t1, t0   # t1 *= t0 (result *= i)
+    addi t0, t0, -1  # t0 -= 1  (i -= 1)
+Test:
+    # BLT take the branch if rs1 is less than rs2 (signed)
+    blt x0, t0, Loop
+
+    add a0, x0, t1
+    jr ra
